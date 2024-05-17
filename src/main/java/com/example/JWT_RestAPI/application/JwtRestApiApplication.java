06d9185137d97d14eb53bd44bc66175aebf9c018
Luciano Romualdo <luciano.oliveira@lynxprocess.com>
2024-05-17 14:38:58 -0300

    feat: corrige projeto

--- src/main/java/com/example/JWT_RestAPI/application/JwtRestApiApplication.java
+++ src/main/java/com/example/JWT_RestAPI/application/JwtRestApiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"com.example"})
 public class JwtRestApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(JwtRestApiApplication.class, args);
 	}
-
 }
